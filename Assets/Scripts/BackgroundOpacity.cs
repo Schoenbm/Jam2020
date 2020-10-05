ba4120a66163ba9c18729fbf8dbb4793af2b3b7b
schoenbm <maxime.schoenberger@edu.esiee.fr>
2020-10-05 23:43:09 +0200

    no

---
+++ Assets/Scripts/BackgroundOpacity.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BackgroundOpacity : MonoBehaviour
+{
+    public Image background;
+    [SerializeField] private float alphaInstructions = 1;
+    [SerializeField] private float alpha = 0.75f;
+
+    public void goInstructions()
+    {
+        var tempColor = background.color;
+
+        tempColor.a = alphaInstructions;
+        background.color = tempColor;
+    }
+
+    public void goMenu()
+    {
+        var tempColor = background.color;
+
+        
+        tempColor.a = alpha;
+        background.color = tempColor;
+    }
+
+}
