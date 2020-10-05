f7f40a682a2cbf0b03d710e4002dff62958ac564
Ricardo Lozano <lozanogil.ricardo@gmail.com>
2020-10-05 13:19:17 -0500

    animations

--- Assets/Scripts/Egg.cs
+++ Assets/Scripts/Egg.cs
@@ -7,9 +7,20 @@ public class Egg : MonoBehaviour
     public float animationLength =2f;
     public AudioSource eggSe;
 
+    private Animator animator;
+
+    public void Start()
+    {
+        animator = GetComponent<Animator>();
+
+        AnimationClip[] clips = animator.runtimeAnimatorController.animationClips;
+        animationLength = clips[1].length; // clips[1] is egg crack
+    }
+
     public void Hatch()
     {
         //TODO: play animation
+        animator.SetBool("isCracking", true);
 
         // destroy egg shell after animation
         eggSe.Play();
